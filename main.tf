6bc00e667e1ef2000911111d8682ad862eed83cc
vema syam <vsyamkumar96@gmail.com>
2026-03-14 23:52:56 -0700

    s3

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "demo" {
-  bucket = "syam-terraform-demo-bucket-12345"
-}
+//resource "aws_s3_bucket" "demo" {
+//  bucket = "syam-terraform-demo-bucket-12345"
+//}
